71b40a90a1894debfb14b85327904d76d29affa2
David Ma <davidma415@gmail.com>
2018-08-09 01:18:06 -0400

    Finished CRUD actions on the dictionary

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -1,25 +1,51 @@
 class EntriesController < ApplicationController
   def index
+    @entries = Entry.all
   end
 
   def show
+    @entry = Entry.find(params[:id])
   end
 
   def new
+    @entry = Entry.new
   end
 
   def edit
+    @entry = Entry.find(params[:id])
   end
 
   def create
-    redirect_to entries_url
+    @entry = Entry.new
+
+    @entry.word = params[:entry][:word]
+    @entry.language = params[:entry][:language]
+    @entry.definition = params[:entry][:definition]
+
+    if @entry.save
+      redirect_to entries_url
+    else
+      render :new
+    end
   end
 
   def update
-    redirect_to entry_url(params[:id])
+    @entry = Entry.find(params[:id])
+
+    @entry.word = params[:entry][:word]
+    @entry.language = params[:entry][:language]
+    @entry.definition = params[:entry][:definition]
+
+    if @entry.save
+      redirect_to entries_url
+    else
+      render :edit
+    end
   end
 
   def destroy
+    @entry = Entry.find(params[:id])
+    @entry.destroy
     redirect_to entries_url
   end
 end
